f3a63bc08ba0aedca234e523e742bead284eb098
Urnguk <spiri-o-wate@mail.ru>
2016-11-24 22:09:48 +0300

    Add files via upload

--- enemies.py
+++ enemies.py
@@ -7,6 +7,22 @@ def iseasy(x):
         if x%i==0:
             return False
     return True
+def chane(x):
+    n=2
+    A=[]
+    if iseasy(x):
+        return str(0)
+    while n<=x**0.5:
+        if x%n==0:
+            A.append(n)
+            x=x//n
+            if iseasy(x):
+                A.append(x)
+                return " ".join(map(str, A))
+        else:
+            n += 1
+
+    return " ".join(map(str, A))
 class Enemy(Attacker):
     def __init__ (self):
         self.experience = (10 * self._attack + self._health)//10
@@ -45,7 +61,7 @@ class GreenDragon(Dragon):
         x = randint(1,100)
         y = randint(1,100)
         self.__quest = str(x) + '+' + str(y)
-        self.set_answer(x + y)
+        self.set_answer(str(x + y))
         return self.__quest
 
 class RedDragon(Dragon):
@@ -59,7 +75,7 @@ class RedDragon(Dragon):
         x = randint(1,100)
         y = randint(1,100)
         self.__quest = str(x) + '-' + str(y)
-        self.set_answer(x - y)
+        self.set_answer(str(x - y))
         return self.__quest
 
 class BlackDragon(Dragon):
@@ -74,7 +90,7 @@ class BlackDragon(Dragon):
         x = randint(1,10)
         y = randint(1,10)
         self.__quest = str(x) + '*' + str(y)
-        self.set_answer(x * y)
+        self.set_answer(str(x * y))
         return self.__quest
 class Troll(Enemy):
     def __init__(self):
@@ -95,7 +111,7 @@ class BrownTroll(Troll):
     def question(self):
         x = randint(1,5)
         self.__quest = "угадай число от 1 до 5"
-        self.set_answer(x)
+        self.set_answer(str(x))
         return self.__quest
 class BlueTroll(Troll):
     def __init__(self):
@@ -106,12 +122,25 @@ class BlueTroll(Troll):
         super().__init__()
     def question(self):
         x = randint(1,100)
-        self.__quest = str(x) + " - это число простое?"
-        self.set_answer(iseasy(x))
+        self.__quest = str(x) + " - это число простое? (0 - нет, 1 - да)"
+        self.set_answer(str(int(iseasy(x))))
+        return self.__quest
+
+class OrangeTroll(Troll):
+    def __init__(self):
+        self._health = 50
+        self._attack = 5
+        self._color = 'оранжевый'
+        super().__init__()
+
+    def question(self):
+        x = randint(3, 50)
+        self.__quest = str(x) + " - напиши множители этого числа! (в порядке возрастания, чере пробел, если простое - напиши 0)"
+        self.set_answer(chane(x))
         return self.__quest
 
 #FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
 # красный дракон учит вычитанию, а чёрный -- умножению.
 
 
-enemy_types = [GreenDragon, RedDragon, BlackDragon, BrownTroll,BlueTroll]
+enemy_types = [RedDragon, GreenDragon, BlackDragon, BlueTroll, BrownTroll, OrangeTroll]
--- tournament.py
+++ tournament.py
@@ -2,12 +2,15 @@
 # license: GPLv3
 from enemies import *
 from hero import *
+from tkinter import *
 
-def annoying_input_int(message =''):
+
+
+def annoying_input(message =''):
     answer = None
     while answer == None:
         try:
-            answer = int(input(message))
+            answer = input(message)
         except ValueError:
             print('Вы ввели недопустимые символы')
     return answer
@@ -22,7 +25,7 @@ def game_tournament(hero, monster_list):
         print('Вышел', monster._color, inz, "!")
         while monster.is_alive() and hero.is_alive():
             print('Вопрос:', monster.question())
-            answer = annoying_input_int('Ответ:')
+            answer = annoying_input('Ответ:')
 
             if monster.check_answer(answer):
                 hero.attack(monster)
@@ -44,8 +47,15 @@ def game_tournament(hero, monster_list):
 def start_game():
 
     try:
-        print('Добро пожаловать в арифметико-ролевую игру с драконами!')
-        print('Представьтесь, пожалуйста: ', end = '')
+        root=Tk()
+        label=Label(root,text='Добро пожаловать в арифметико-ролевую игру с драконами!')
+        label.pack()
+
+        label = Label(root, text='Представьтесь, пожалуйста: ')
+        label.pack()
+
+
+        root.mainloop()
         hero = Hero(input())
 
         monster_number = 5
@@ -53,6 +63,7 @@ def start_game():
         assert(len(monster_list) == 5)
         print('У Вас на пути', monster_number, 'монстров!')
         game_tournament(hero, monster_list)
+
 
     except EOFError:
         print('Поток ввода закончился. Извините, принимать ответы более невозможно.')
